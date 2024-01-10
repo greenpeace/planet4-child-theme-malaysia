fce6e73eec86b97c5b0406e26e2dd9940041f883
Nut Wisanukitpaiboon <156182420+tall-nutw@users.noreply.github.com>
2024-01-10 16:45:26 +0700

    Update functions.php to integrate with Fundraise Up

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@
  */
 
 add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
+add_action('wp_head', 'include_fundraiseup_script');
 
 function enqueue_child_styles() {
 	$css_creation = filectime(get_stylesheet_directory() . '/style.css');
@@ -15,3 +16,17 @@ function enqueue_child_styles() {
 add_filter( 'locale', function() {
 	return 'en';
 });
+
+function include_fundraiseup_script() {
+?>
+<!-- Fundraise Up: the new standard for online giving -->
+<script>(function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track'
+.split(','),i,o=function(n){return'function'==typeof n?o.l.push([arguments])&&o
+:function(){return o.l.push([n,arguments])&&o}},t=d.getElementsByTagName(s)[0],
+j=d.createElement(s);j.async=!0;j.src='https://cdn.fundraiseup.com/widget/'+a;
+t.parentNode.insertBefore(j,t);o.s=Date.now();o.v=4;o.h=w.location.href;o.l=[];
+for(i=0;i<7;i++)o[l[i]]=o(l[i]);w[n]=o}
+})(window,document,'script','FundraiseUp','AZVBNGQU');</script>
+<!-- End Fundraise Up -->
+<?php
+}
